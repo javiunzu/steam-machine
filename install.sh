71f85d7e808d1b4d218b104c5ed89d57a8341679
javiunzu <javiunzu@gmail.com>
2020-05-30 00:19:12 +0200

    Added audio and increased video memory. Added network manager and fixed issue with xorg input.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ set -eu
 apt update
 apt upgrade -y
 echo steam steam/question select "I AGREE" | debconf-set-selections
-DEBIAN_FRONTEND=noninteractive apt install -y kde-plasma-desktop kdeconnect htop tree tmux python3-pip p7zip-full clamtk apparmor apparmor-utils apparmor-profiles apparmor-profiles-extra firejail firefox-esr steam mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+DEBIAN_FRONTEND=noninteractive apt install -y kde-plasma-desktop xserver-xorg-input-all network-manager kdeconnect htop tree tmux python3-pip p7zip-full clamtk apparmor apparmor-utils apparmor-profiles apparmor-profiles-extra firejail firefox-esr steam mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
